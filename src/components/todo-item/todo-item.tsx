fe904bf45c63760b76da46733ad59a10315016f1
Adrian Lafond <mail@adrianlafond.com>
2021-02-20 11:44:48 -0500

    mess with the item text

--- src/components/todo-item/todo-item.tsx
+++ src/components/todo-item/todo-item.tsx
@@ -26,7 +26,8 @@ export const TodoItem: React.FC<TodoItemProps> = ({
   return (
     <div className="todo-item">
       <TodoCheck checked={complete} onChange={onChange} />
-      Todo Item
+      {/* <div className="todo-item__text">Text</div> */}
+      <div className="todo-item__text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. </div>
     </div>
   );
 }
