dfea6eae01975bf6d5a74e8dd6b0558fcc0872c5
Adrian Lafond <mail@adrianlafond.com>
2021-02-20 11:11:52 -0500

    add focus styles

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
   return (
     <main className="app">
       <TodoItem id="item0" complete={items['item0'] && items['item0'].complete} onCompleteChange={onCompleteChange} />
+      <TodoItem id="item1" complete={items['item1'] && items['item1'].complete} onCompleteChange={onCompleteChange} />
     </main>
   );
 }
