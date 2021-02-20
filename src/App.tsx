6b4932f11ab16870edda2b7b8c228971b2095981
Adrian Lafond <mail@adrianlafond.com>
2021-02-20 16:37:40 -0500

    handle todo text change

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,57 @@
 import React from 'react';
 
-import { TodoItem, CompleteChangeEvent } from './components/todo-item';
+import { TodoItem } from './components/todo-item';
 import { TodoAddItem } from './components/todo-add-item';
 import './app.css';
 
-interface ItemsState { [key: string]: { complete: boolean }};
+interface Item { id: string; complete: boolean; text: string; };
+
+let uid = 2;
 
 function App() {
-  const [items, setItems] = React.useState<ItemsState>({});
+  const [items, setItems] = React.useState<Item[]>([
+    { id: 'item0', complete: false, text: 'Hello, world' },
+    { id: 'item1', complete: false, text: 'Buy milk' },
+  ]);
 
-  function onCompleteChange({ id, complete }: CompleteChangeEvent) {
-    setItems({ ...items, [id]: { complete } });
+  function onTodoComplete(id: string, complete: boolean) {
+    const newItems = items.slice(0);
+    newItems[items.findIndex(item => item.id === id)].complete = complete;
+    setItems(newItems);
+  }
+
+  function onTodoDelete(id: string) {
+    const newItems = items.slice(0);
+    newItems.splice(items.findIndex(item => item.id === id), 1);
+    setItems(newItems);
+  }
+
+  function onTodoText(id: string, text: string) {
+    const newItems = items.slice(0);
+    newItems[items.findIndex(item => item.id === id)].text = text;
+    setItems(newItems);
   }
 
   function onAddItem() {
-    console.log('add');
+    const newItems = items.slice(0);
+    newItems.push({ id: `item${uid++}`, complete: false, text: '' });
+    setItems(newItems);
   }
 
   return (
     <main className="app">
-      <TodoItem id="item0" complete={items['item0'] && items['item0'].complete} onCompleteChange={onCompleteChange}>
-        Text
-      </TodoItem>
-      <TodoItem id="item1" complete={items['item1'] && items['item1'].complete} onCompleteChange={onCompleteChange}>
-        Hello, world
-      </TodoItem>
+      {items.map(item => (
+        <TodoItem
+          key={item.id}
+          id={item.id}
+          complete={item.complete}
+          onCompleteChange={onTodoComplete}
+          onDelete={onTodoDelete}
+          onTextChange={onTodoText}
+        >
+          {item.text}
+        </TodoItem>
+      ))}
       <TodoAddItem onAdd={onAddItem} />
     </main>
   );
--- src/components/todo-item/todo-item.tsx
+++ src/components/todo-item/todo-item.tsx
@@ -1,36 +1,66 @@
 import React from 'react';
-import { TodoCheck } from '../todo-check';
 
 import './style.css';
 
 export interface TodoItemProps {
-  children?: React.ReactNode | React.ReactNode[];
-  complete?: boolean;
-  id: string;
-  onCompleteChange: (args: CompleteChangeEvent) => void;
-}
-
-export interface CompleteChangeEvent {
-  id: string;
+  children?: React.ReactText;
   complete: boolean;
+  id: string;
+  onCompleteChange?: (id: string, complete: boolean) => void;
+  onDelete?: (id: string) => void;
+  onTextChange?: (id: string, text: string) => void;
 }
 
 export const TodoItem: React.FC<TodoItemProps> = ({
   children,
   complete = false,
   id,
-  onCompleteChange,
+  onCompleteChange = () => undefined,
+  onDelete = () => undefined,
+  onTextChange = () => undefined,
 }) => {
-  function onChange(checked: boolean) {
-    onCompleteChange({ id, complete: checked });
+  const [isConfirmingDelete, setIsConfirmingDelete] = React.useState(false);
+
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    onCompleteChange(id, event.target.checked);
+  }
+
+  function onDeleteClick() {
+    setIsConfirmingDelete(true);
+  }
+
+  function onCancelDelete() {
+    setIsConfirmingDelete(false);
+  }
+
+  function onConfirmDelete() {
+    onDelete(id);
+  }
+
+  function onInputRef(el: HTMLInputElement) {
+    if (el) {
+      el.focus();
+    }
+  }
+
+  function onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    onTextChange(id, event.target.value);
   }
 
   return (
     <div className="todo-item">
-      <TodoCheck checked={complete} onChange={onChange} />
-      <div className="todo-item__text">
-        {children}
-      </div>
+      {isConfirmingDelete ? (
+        <>
+          <button onClick={onCancelDelete}>Cancel</button>
+          <button onClick={onConfirmDelete}>Yes, delete</button>
+        </>
+      ) : (
+        <>
+            <input type="checkbox" checked={complete} onChange={onChange} className="todo-item__complete" />
+            <input type="text" className="todo-item__text" value={children} ref={onInputRef} onChange={onInputChange} />
+            <button className="todo-item__delete" onClick={onDeleteClick}>&#x1f5d1;</button>
+        </>
+      )}
     </div>
   );
 }
