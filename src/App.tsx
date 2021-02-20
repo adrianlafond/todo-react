840b481ac761fc473026f016e152f3cee0525550
Adrian Lafond <mail@adrianlafond.com>
2021-02-20 15:33:56 -0500

    create add item button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { TodoItem, CompleteChangeEvent } from './components/todo-item';
+import { TodoAddItem } from './components/todo-add-item';
 import './app.css';
 
 interface ItemsState { [key: string]: { complete: boolean }};
@@ -12,6 +13,10 @@ function App() {
     setItems({ ...items, [id]: { complete } });
   }
 
+  function onAddItem() {
+    console.log('add');
+  }
+
   return (
     <main className="app">
       <TodoItem id="item0" complete={items['item0'] && items['item0'].complete} onCompleteChange={onCompleteChange}>
@@ -20,6 +25,7 @@ function App() {
       <TodoItem id="item1" complete={items['item1'] && items['item1'].complete} onCompleteChange={onCompleteChange}>
         Hello, world
       </TodoItem>
+      <TodoAddItem onAdd={onAddItem} />
     </main>
   );
 }
